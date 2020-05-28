b84479e32e6cd327b67c3f4aa46f30d435bc25ac
miltoncosme <milton.cosme@gmail.com>
2020-05-28 12:02:11 -0300

    first commit

---
+++ Plugin/Checkout/LayoutProcessorPlugin.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Magenteiro\StreetLines\Plugin\Checkout;
+
+/**
+ * Class LayoutProcessorPlugin
+ * Based on Eduardo's solution. See  https://magento.stackexchange.com/a/188678/555.
+ *
+ * @author    Ricardo Martins
+ * @copyright 2019 Magenteiro.com
+ * @package   Magenteiro\StreetLines\Plugin\Checkout
+ */
+class LayoutProcessorPlugin
+{
+    /**
+     * @param \Magento\Checkout\Block\Checkout\LayoutProcessor $subject
+     * @param array $jsLayout
+     * @return array
+     */
+
+    public function afterProcess(
+        \Magento\Checkout\Block\Checkout\LayoutProcessor $subject,
+        array  $jsLayout
+    ) {
+        $jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']
+        ['shippingAddress']['children']['shipping-address-fieldset']['children']['street'] = [
+            'component' => 'Magento_Ui/js/form/components/group',
+            'label' => __('Street Address'),
+            'required' => true,
+            'dataScope' => 'shippingAddress.street',
+            'provider' => 'checkoutProvider',
+            'sortOrder' => 0,
+            'type' => 'group',
+            'additionalClasses' => 'street',
+            'children' => [
+                [
+                    'label' => __('Street'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '0',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+                [
+                    'label' => __('Street Number'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '1',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => true, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+                [
+                    'label' => __('Address complement'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '2',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => false, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+                [
+                    'label' => __('Neighborhood'),
+                    'component' => 'Magento_Ui/js/form/element/abstract',
+                    'config' => [
+                        'customScope' => 'shippingAddress',
+                        'template' => 'ui/form/field',
+                        'elementTmpl' => 'ui/form/element/input'
+                    ],
+                    'dataScope' => '3',
+                    'provider' => 'checkoutProvider',
+                    'validation' => ['required-entry' => false, "min_text_len‌​gth" => 1, "max_text_length" => 255],
+                ],
+            ]
+        ];
+        return $jsLayout;
+    }
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magenteiro_StreetLines',
+    __DIR__
+);
